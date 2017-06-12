bd07ab043572a242fbd8b50ae4acc51a97283f06
Nicholas Sardo <gcc.programmer@gmail.com>
2017-06-12 02:50:18 -0700

    forgot to purge commented out pre-generated code

--- imports/ui/main.js
+++ imports/ui/main.js
@@ -7,7 +7,7 @@ import './post.js';
 import './messages';
 
 Template.main.onCreated(function mainOnCreated() {
-  // this.counter = new ReactiveVar(0);
+
 });
 
 Template.main.rendered = function() {
@@ -16,18 +16,9 @@ Template.main.rendered = function() {
 
 
 Template.main.helpers({
-  /*
-  counter() {
-    return Template.instance().counter.get();
-  },
-  */
+  
 });
 
 Template.main.events({
-  /*
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-  */
+
 });
